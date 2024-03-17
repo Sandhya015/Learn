59337ed7d2cceed9428083185731e2e80e5ac9dd
Sandhya015 <sandhyanaik3249@gmail.com>
2024-03-17 12:25:34 +0530

    updated strings

--- quiz/home/views.py
+++ quiz/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello from Sandhya")
+    return HttpResponse("Hello from Sandhya Manjunath Naik")
