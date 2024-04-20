8c2d6a52398cc9863b286dad7c65b998a53a6fd2
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-04-20 10:09:22 +0530

    Update views.py

--- quiz/home/views.py
+++ quiz/home/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world welcome back to my channel")
+    return HttpResponse("Hello world this universe is soo bigg")
