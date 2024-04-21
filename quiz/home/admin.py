7828935c1121c655e3dbbaf3fa557fd29fe38b5a
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-04-21 21:00:18 +0530

    Update admin.py

--- quiz/home/admin.py
+++ quiz/home/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import *
 
 admin.site.register(Category)
-admin.site.register(Question)
+
+class AnswerAdmin(admin.StackedInline):
+    model = Answer
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerAdmin]
+
+admin.site.register(Question, QuestionAdmin)  
 admin.site.register(Answer)
