76c4d8253b9bc5f48fd14a291e14a395029b7027
Sandhya <144054124+Sandhya015@users.noreply.github.com>
2024-04-22 00:30:58 +0530

    Update views.py

--- quiz/home/views.py
+++ quiz/home/views.py
@@ -1,5 +1,32 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.http import HttpResponse, JsonResponse  # Import JsonResponse
+from .models import *
+import random
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello world this universe is soo bigg")
+    return HttpResponse("Hello world")
+
+def get_quiz(request):
+    try:
+        question_objs = Question.objects.all()
+        data = []
+        
+
+        for question_obj in question_objs:
+            print(question_obj.get_answers())
+            data.append({
+                "category": question_obj.category.category_name,
+                "question": question_obj.question,
+                "marks": question_obj.marks,
+                "answers":question_obj.get_answers()
+
+            })
+        
+        payload = {'status': True, 'data': data}  # Added comma after 'status': True
+
+        return JsonResponse(payload)
+    
+    except Exception as e:
+        print(e)
+        return HttpResponse("Something went wrong")
